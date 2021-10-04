806a332955ac7ae1cad9f605394a12088b0f4e8b
遠藤拓弥 <entaku19890818@gmail.com>
2021-10-04 14:14:40 +0900

    Initial Commit

---
+++ download-cleaner/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  download-cleaner
+//
+//  Created by 遠藤拓弥 on 2021/10/04.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
